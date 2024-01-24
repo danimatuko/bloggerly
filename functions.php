b0c6f0de2c5d4fe77568193c1c4467495e344a54
danimatuko <danimatuko@outlook.com>
2024-01-24 17:48:49 +0200

    Refactor header and footer into separate template parts and update functions.php

--- functions.php
+++ functions.php
@@ -2,9 +2,10 @@
 // Variables
 
 // Includes
-include get_theme_file_path('/inc/enqueue.php');
-include get_theme_file_path('/inc/bloggery_head.php');
+include get_theme_file_path('inc/enqueue.php');
+include get_theme_file_path('inc/bloggery_fonts_preconnect.php');
 
 // Hooks
 add_action('wp_enqueue_scripts', 'bloggery_styles');
+// add preconnect links to the head tag
 add_action('wp_head', 'bloggery_fonts_preconnect', 5);
