ef88676f1e280295ee8ba6b1c7e0036afe65df1f
danimatuko <danimatuko@outlook.com>
2024-01-25 20:54:24 +0200

    Added theme support for editor styles, updated header layout and added new CSS rules for editor and public index

--- functions.php
+++ functions.php
@@ -4,8 +4,10 @@
 // Includes
 include get_theme_file_path('inc/enqueue.php');
 include get_theme_file_path('inc/bloggery_fonts_preconnect.php');
+include get_theme_file_path('inc/bloggery_setup.php');
 
 // Hooks
 add_action('wp_enqueue_scripts', 'bloggery_styles');
 // add preconnect links to the head tag
 add_action('wp_head', 'bloggery_fonts_preconnect', 5);
+add_action('after_setup_theme', 'bloggery_setup');
